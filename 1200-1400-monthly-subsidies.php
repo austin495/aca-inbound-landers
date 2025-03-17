95f633c9b7c3e4b07c3395b6c172353a5b342eb3
Austin Norman <asimnoman2000@gmail.com>
2025-03-18 01:24:25 +0500

    commit

---
+++ 1200-1400-monthly-subsidies.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ACA Inbounds - $1200 $1400 Monthly Subsidies</title>
+    <link rel="stylesheet" href="1200-1400-monthly-subsidies.css">
+</head>
+<body>
+    <section class="hero-section">
+        <div class="hero container">
+            <div class="hero-text">
+                <h3 class="upper-heading">Last updated: 3/14/2025</h3>
+                <h1><span style="color: #E5F45A;">Good News!</span> All Americans Will Receive <span style="text-decoration: underline; text-decoration-color: #E5F45A;">$1,200 - $1,400</span> in Monthly Subsidies to spend on <br><span style="color: #E5F45A;">Rent, Gas, Groceries, or anything else.</span></h1>
+            </div>
+            <div class="hero-img">
+                <img src="/images/hero-img-1.webp" alt="Credit Card">
+            </div>
+        </div>
+    </section>
+
+    <section class="form-heading-section">
+        <div class="form-heading container">
+            <div class="form-heading">
+                <h2>Secure a <strong>$1,200 - $ 1,400 Monthly Subsidy</strong> by registering Now.</h2>
+            </div>
+        </div>
+    </section>
+
+    <section class="form-main-section" id="form">
+        <div class="form container">
+            <div class="form-main">
+                <!-- Step 1 -->
+                <div class="step-1" id="step-1">
+                    <button class="form-btn" id="qualify">See If You Qualify!</button>
+                    <p>Keep more in your pocket - have more money to spend on anything.</p>
+                </div>
+
+                <!-- Step 2 -->
+                <div class="step-2" id="step-2">
+                    <div class="step-inner-2">
+                        <h3 class="question">Do you make less than $50,000 a year?</h3>
+                        <p class="explainer">TO QUALIFY, YOUR ANNUAL INCOME MUST BE UNDER $50,000.</p>
+                        <button class="form-btn" id="step-2-yes">Yes</button>
+                        <button class="form-btn-no" id="step-2-no">No</button>
+                    </div>
+                </div>
+
+                <!-- Step 3 -->
+                <div class="step-3" id="step-3">
+                    <div class="step-inner-3">
+                        <h3 class="question">Are you currently receiving Medicare or Medicaid?</h3>
+                        <p class="explainer">TO QUALIFY, YOU MUST NOT BE ENROLLED IN MEDICARE OR MEDICAID.</p>
+                        <button class="form-btn" id="step-3-no">No</button>
+                        <button class="form-btn-no" id="step-3-yes">Yes</button>
+                    </div>
+                </div>
+
+                <!-- Step 4 -->
+                <div class="step-4" id="step-4">
+                    <div class="step-inner-4">
+                        <h3 class="question">What is your age?</h3>
+                        <p class="explainer">TO QUALIFY, YOU MUST BE BETWEEN 18 AND 64 YEARS OLD.</p>
+                        <button class="form-btn" id="step-4-18-64">18 to 64</button>
+                        <button class="form-btn-no" id="step-4-over-65">Over 65</button>
+                    </div>
+                </div>
+
+                <!-- Last Step -->
+                <div class="last-step" id="last-step">
+                    <div class="last-step-inner">
+                        <h3><span style="text-decoration: underline; text-decoration-color: #E5F45A;"><span style="color: #E5F45A;">$6,400</span> in Healthcare</span></h3>
+                        <p class="after-heading">Assistance Now Available to Eligible Individuals</p>
+                        <p class="thank-you">Thank<span style="color: #E5F45A;">You!</span></p>
+                        <p class="after-thankYou">You pre-qualify! Call now to claim your <b>$6,400</b> coverage and <b>$0</b> Health Plan.</p>
+                        <button class="form-btn" id="click-to-call"><a class="ctca" href="tel:+18447804289">Click To Call Now</a></button>
+                        <p class="after-call-btn" id="call-now">Call Now <a href="tel:+18447804289">+1 844-780-4289</a></p>
+                        <p class="live-agents">Live Wait Time: <span id="fiveMintTimer">0</span> Minutes</p>
+                        <p class="agents">Available Agents: 4</p>
+                    </div>
+                </div>
+
+                <!-- Form Step -->
+                <div class="form-step" id="form-step">
+                    <div class="form-step-inner">
+                        <h3><span style="text-decoration: underline; text-decoration-color: #E5F45A;"><span style="color: #E5F45A;">$6,400</span> in Healthcare</span></h3>
+                        <p class="after-heading">Assistance Now Available to Eligible Individuals</p>
+                        <form action="">
+                            <label for="FirstName">First Name <span style="color: red;">*</span></label>
+                            <input type="text" id="FirstName" required>
+
+                            <label for="LastName">Last Name <span style="color: red;">*</span></label>
+                            <input type="text" id="LastName" required>
+
+                            <label for="PhoneNumber">Phone Number <span style="color: red;">*</span></label>
+                            <input type="tel" id="PhoneNumber" required>
+
+                            <label for="DOB">Date of Birth <span style="color: red;">*</span></label>
+                            <input type="date" id="DOB" required>
+
+                            <label for="ZipCode">Zip Code <span style="color: red;">*</span></label>
+                            <input type="text" id="ZipCode" required>
+
+                            <input type="checkbox" name="consent" id="Consent" required>
+                            <label for="Consent">I agree to the <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.</label>
+
+                            <button class="submit-btn" id="submit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="footer-section">
+        <div class="footer container">
+            <div class="footer-text">
+                <h2>TERMS OF SERVICE:</h2>
+                <p>Welcome to ACAPlanAssist.com. We are a referral source designed to provide information and connect consumers with companies that may offer certain insurance coverage options. ACAPlanAssist.com does not act as an insurance broker, agent, or advisor, nor do we make decisions regarding insurance coverage availability or eligibility. The helpline provided on our platform is free to call; however, the services or insurance programs you choose to pursue may involve costs. Neither ACAPlanAssist.com nor any supplemental insurance plans or providers to which you may be connected are endorsed by the U.S. government or the Medicare program. We do not guarantee a specific outcome or result from using our services.</p>
+                <p>By submitting your information, you consent to being contacted by one of our agents to discuss your request, gather feedback, or provide assistance with products, services, or plans. These communications may occur via the phone number you provided, even if you are on a do-not-call list. If we are unable to reach you by phone, we may send a text message notifying you of our attempt to contact you. Phone calls and text messages may be sent using automated technology, and standard carrier fees may apply to text messages. You may opt out of communications at any time by informing our customer service team or replying with “STOP” to text messages. By using ACAPlanAssist.com, you acknowledge and agree to these terms. For any questions or assistance, please contact our customer service team.</p>
+                <h3>Connect with Us Now: <a href="tel:+18447804289">+1 844-780-4289</a></h3>
+            </div>
+        </div>
+    </section>
+
+    <!-- Script -->
+    <script src="script.js"></script>
+</body>
+</html>
