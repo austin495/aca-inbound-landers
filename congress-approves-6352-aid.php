caa6d434349421df4dfb435bea11f8814631c263
Austin Norman <asimnoman2000@gmail.com>
2025-03-19 00:46:32 +0500

    commit

--- congress-approves-6352-aid.php
+++ congress-approves-6352-aid.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ACA Inbounds</title>
+    <title>ACA Inbounds - Congress Approves $6352 Aid</title>
     <link rel="stylesheet" href="/congress-approves-6352-aid.css">
 </head>
 <body>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ACA Inbounds</title>
+    <title>ACA Inbounds - 2025 Subsidy Program Get Up to $6,140 in Savings!</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
