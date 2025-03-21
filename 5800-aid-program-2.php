fec61d72ac42e98667b43fd8f086ca11d12d35b5
Austin Norman <asimnoman2000@gmail.com>
2025-03-22 02:19:16 +0500

    commit

--- 5800-aid-program-2.php
+++ 5800-aid-program-2.php
@@ -552,7 +552,6 @@
           buttonValue = $(this).attr("data-form-value");
 
           if (currentStep == 1) {
-            //scrollToBottom();
             $("#agentBlock2 .agent-chat").prepend(typingEffect());
             $("#msg4").addClass("hidden");
             $("#userBlock1").removeClass("hidden");
@@ -579,86 +578,38 @@
           }
 
           if (currentStep == 2) {
-            //scrollToBottom();
             $("#agentBlock3 .agent-chat").prepend(typingEffect());
             $("#msg8").addClass("hidden");
             $("#userBlock2").removeClass("hidden");
-            //scrollToBottom();
+
             if (buttonValue == "No") {
-              /*$("#msg9no").removeClass("hidden");
-              $("#hdnApprovalStatus").val("no");
-              $("#msg12medicare").removeClass("hidden");
-              setTimeout(function () {
-                $("#agentBlock4").removeClass("hidden");
-                scrollToBottom();
-                setTimeout(function () {
-                  $(".temp-typing").remove();
-                  $("#msg18").removeClass("hidden").after(typingEffect());
-                  scrollToBottom();
-                  setTimeout(function () {
-                    $(".temp-typing").remove();
-                    $("#msg19").removeClass("hidden");
-                    scrollToBottom();
-                    setTimeout(function () {
-                      $("#disconnected").removeClass("hidden");
-                      scrollToBottom();
-                    }, 1250);
-                  }, 1250);
-                }, 1250);
-              }, 1250);
-              return false;*/
               $("#msg9no").removeClass("hidden");
-              scrollToBottom();
-              setTimeout(function () {
-                $("#agentBlock3").removeClass("hidden");
-                scrollToBottom();
-                setTimeout(function () {
-                  $(".temp-typing").remove();
-                  $("#msg10").removeClass("hidden").after(typingEffect());
-                  scrollToBottom();
-                  setTimeout(function () {
-                    $(".temp-typing").remove();
-                    $("#msg11").removeClass("hidden");
-                    scrollToBottom();
-                  }, 0);
-                }, 1750);
-              }, 50);
             } else {
               $("#msg9yes").removeClass("hidden");
+            }
+
+            scrollToBottom();
+            setTimeout(function () {
+              $("#agentBlock3").removeClass("hidden");
               scrollToBottom();
               setTimeout(function () {
-                $("#agentBlock3").removeClass("hidden");
+                $(".temp-typing").remove();
+                $("#msg10").removeClass("hidden").after(typingEffect());
                 scrollToBottom();
                 setTimeout(function () {
                   $(".temp-typing").remove();
-                  $("#msg10").removeClass("hidden").after(typingEffect());
+                  $("#msg11").removeClass("hidden");
                   scrollToBottom();
-                  setTimeout(function () {
-                    $(".temp-typing").remove();
-                    $("#msg11").removeClass("hidden");
-                    scrollToBottom();
-                  }, 0);
-                }, 1750);
-              }, 50);
-            }
+                }, 0);
+              }, 1750);
+            }, 50);
           }
 
           if (currentStep == 3) {
-            function syes() {
-              alert("Yes");
-            }
-
-            function sno() {
-              alert("no");
-            }
-
-            //scrollToBottom();
-            $("#agentBlock4 .agent-chat .agent-chat-2 #agentBlock5").prepend(
-              typingEffect()
-            );
+            $("#agentBlock4 .agent-chat").prepend(typingEffect());
             $("#msg11").addClass("hidden");
             $("#userBlock3").removeClass("hidden");
-            //scrollToBottom();
+
             if (buttonValue == "No") {
               $("#msg12no").removeClass("hidden");
               setTimeout(function () {
@@ -683,7 +634,6 @@
                         setTimeout(function () {
                           $(".temp-typing").remove();
                           $("#msg17").removeClass("hidden");
-                          //$('#tcpa').removeClass('hidden');
                           scrollToBottom();
                           afterDelay(execute, 120000);
                         }, 0);
@@ -693,49 +643,18 @@
                 }, 1250);
               }, 250);
             } else if (buttonValue == "Yes") {
-              /*$("#msg12yes").removeClass("hidden");
-              setTimeout(function () {
-                $("#agentBlock5").removeClass("hidden");
-                scrollToBottom();
-                setTimeout(function () {
-                  $(".temp-typing").remove();
-                  $("#msg20").removeClass("hidden").after(typingEffect());
-                  scrollToBottom();
-                  setTimeout(function () {
-                    $(".temp-typing").remove();
-                    $("#msg21").removeClass("hidden").after(typingEffect());
-                    scrollToBottom();
-                    setTimeout(function () {
-                      $(".temp-typing").remove();
-                      $("#msg22").removeClass("hidden").after(typingEffect());
-                      scrollToBottom();
-                      setTimeout(function () {
-                        $(".temp-typing").remove();
-                        $("#msg23").removeClass("hidden").after(typingEffect());
-                        scrollToBottom();
-                        setTimeout(function () {
-                          $(".temp-typing").remove();
-                          $("#msg24").removeClass("hidden");
-                          //$('#tcpa').removeClass('hidden');
-                          scrollToBottom();
-                        }, 0);
-                      }, 2250);
-                    }, 1250);
-                  }, 1750);
-                }, 1250);
-              }, 250);*/
               $("#msg12yes").removeClass("hidden");
-              $(".form-main").removeClass("hidden");
+
+              // Check if Step 2 was 'No' or Step 3 is 'Yes'
+              const step2Answer = $("#msg9no").is(":visible") ? "no" : "yes";
+              if (step2Answer === "no" || buttonValue === "Yes") {
+                $(".form-main").removeClass("hidden");
+                $("#agentBlock4, #agentBlock5").addClass("hidden");
+                scrollToBottom();
+              }
             }
 
             if ($("#hdnApprovalStatus").val() == "no") {
-              //downsell
-              /*$("#DownsellNumber").removeClass("hidden");
-              $("#DownsellNumber1").removeClass("hidden");
-              $("#msg14disapproved").removeClass("hidden");
-              $("#msg16disapproved").removeClass("hidden");
-              $("#msg21disapproved").removeClass("hidden");
-              $("#msg23disapproved").removeClass("hidden");*/
               $("#PrimaryNumber").removeClass("hidden");
               $("#PrimaryNumber1").removeClass("hidden");
               $("#msg14approved").removeClass("hidden");
@@ -743,7 +662,6 @@
               $("#msg21approved").removeClass("hidden");
               $("#msg23approved").removeClass("hidden");
             } else {
-              //standard aca
               $("#PrimaryNumber").removeClass("hidden");
               $("#PrimaryNumber1").removeClass("hidden");
               $("#msg14approved").removeClass("hidden");
